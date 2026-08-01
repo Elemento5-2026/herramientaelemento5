82e8d6a70ecd500a1309ac990f475883f5388fb8
Elemento5-2026 <elemento5ag2026@outlook.com>
2026-08-01 00:14:43 -0600

    Update AppNavigator.jsx

--- src/navigation/AppNavigator.jsx
+++ src/navigation/AppNavigator.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Login from "../pages/Login/Login";
 import Dashboard from "../pages/Dashboard/Dashboard";
-import Investigaciones from "../pages/Investigaciones/Investigaciones";
+import Investigaciones from "../pages/Investigaciones/Investigaciones.jsx";
 
 export default function AppNavigator() {
 
