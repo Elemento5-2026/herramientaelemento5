384d4a9b158defa6da66555af82301e3f44adbff
Elemento5-2026 <elemento5ag2026@outlook.com>
2026-08-01 00:35:00 -0600

    Add NuevaInvestigacion component with layout and wizard

---
+++ src/pages/NuevaInvestigacion/NuevaInvestigacion.css
@@ -0,0 +1,78 @@
+import "./NuevaInvestigacion.css";
+
+import Layout from "../../components/Layout";
+import Header from "../../components/Header";
+import Sidebar from "../../components/Sidebar";
+
+export default function NuevaInvestigacion({ setScreen }) {
+
+    return (
+
+        <Layout
+
+            header={<Header />}
+
+            sidebar={
+                <Sidebar
+                    screen="investigaciones"
+                    setScreen={setScreen}
+                />
+            }
+
+        >
+
+            <div className="nueva-investigacion">
+
+                <h1>Nueva investigación</h1>
+
+                <p>
+                    Complete la información del registro del incidente.
+                </p>
+
+                <div className="wizard">
+
+                    <div className="wizard-item active">
+
+                        1. Registro
+
+                    </div>
+
+                    <div className="wizard-item">
+
+                        2. Identificación
+
+                    </div>
+
+                    <div className="wizard-item">
+
+                        3. Análisis de causa
+
+                    </div>
+
+                    <div className="wizard-item">
+
+                        4. Acciones inmediatas
+
+                    </div>
+
+                    <div className="wizard-item">
+
+                        5. Plan de acción
+
+                    </div>
+
+                    <div className="wizard-item">
+
+                        6. Evaluación de eficacia
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </Layout>
+
+    );
+
+}
