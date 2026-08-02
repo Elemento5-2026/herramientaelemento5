3bde907b4bc987405b01f05cc542f4f94a09acfc
Elemento5-2026 <elemento5ag2026@outlook.com>
2026-08-01 19:43:34 -0600

    Update investigacionesService.js

--- src/services/investigacionesService.js
+++ src/services/investigacionesService.js
@@ -1,91 +1,38 @@
 import supabase from "../lib/supabase";
 
-export async function guardarEncabezado(formulario, investigacionId = null) {
+export async function guardarEncabezado(formulario) {
 
-    if (!investigacionId) {
+  const { data, error } = await supabase
+    .from("investigaciones")
+    .insert([{
+      codigo_controlado: formulario.codigo_controlado,
+      estado: "Borrador",
 
-        const { data, error } = await supabase
+      participantes: formulario.participantes,
 
-            .from("investigaciones")
+      elaborado_nombre: formulario.elaborado_nombre,
+      elaborado_puesto: formulario.elaborado_puesto,
+      elaborado_gerencia: formulario.elaborado_gerencia,
+      elaborado_area: formulario.elaborado_area,
+      elaborado_fecha: formulario.elaborado_fecha,
 
-            .insert({
+      revisado_nombre: formulario.revisado_nombre,
+      revisado_puesto: formulario.revisado_puesto,
+      revisado_gerencia: formulario.revisado_gerencia,
+      revisado_area: formulario.revisado_area,
+      revisado_fecha: formulario.revisado_fecha,
 
-                codigo_controlado: formulario.codigo_controlado,
+      aprobado_nombre: formulario.aprobado_nombre,
+      aprobado_puesto: formulario.aprobado_puesto,
+      aprobado_gerencia: formulario.aprobado_gerencia,
+      aprobado_area: formulario.aprobado_area,
+      aprobado_fecha: formulario.aprobado_fecha
+    }])
+    .select()
+    .single();
 
-                estado: "Borrador",
+  if (error) throw error;
 
-                participantes: formulario.participantes,
-
-                elaborado_nombre: formulario.elaborado_nombre,
-                elaborado_puesto: formulario.elaborado_puesto,
-                elaborado_gerencia: formulario.elaborado_gerencia,
-                elaborado_area: formulario.elaborado_area,
-                elaborado_fecha: formulario.elaborado_fecha,
-
-                revisado_nombre: formulario.revisado_nombre,
-                revisado_puesto: formulario.revisado_puesto,
-                revisado_gerencia: formulario.revisado_gerencia,
-                revisado_area: formulario.revisado_area,
-                revisado_fecha: formulario.revisado_fecha,
-
-                aprobado_nombre: formulario.aprobado_nombre,
-                aprobado_puesto: formulario.aprobado_puesto,
-                aprobado_gerencia: formulario.aprobado_gerencia,
-                aprobado_area: formulario.aprobado_area,
-                aprobado_fecha: formulario.aprobado_fecha
-
-            })
-
-            .select()
-
-            .single();
-
-        if (error) throw error;
-
-        return data;
-
-    }
-
-    const { data, error } = await supabase
-
-        .from("investigaciones")
-
-        .update({
-
-            codigo_controlado: formulario.codigo_controlado,
-
-            participantes: formulario.participantes,
-
-            elaborado_nombre: formulario.elaborado_nombre,
-            elaborado_puesto: formulario.elaborado_puesto,
-            elaborado_gerencia: formulario.elaborado_gerencia,
-            elaborado_area: formulario.elaborado_area,
-            elaborado_fecha: formulario.elaborado_fecha,
-
-            revisado_nombre: formulario.revisado_nombre,
-            revisado_puesto: formulario.revisado_puesto,
-            revisado_gerencia: formulario.revisado_gerencia,
-            revisado_area: formulario.revisado_area,
-            revisado_fecha: formulario.revisado_fecha,
-
-            aprobado_nombre: formulario.aprobado_nombre,
-            aprobado_puesto: formulario.aprobado_puesto,
-            aprobado_gerencia: formulario.aprobado_gerencia,
-            aprobado_area: formulario.aprobado_area,
-            aprobado_fecha: formulario.aprobado_fecha,
-
-            updated_at: new Date()
-
-        })
-
-        .eq("id", investigacionId)
-
-        .select()
-
-        .single();
-
-    if (error) throw error;
-
-    return data;
+  return data;
 
 }
