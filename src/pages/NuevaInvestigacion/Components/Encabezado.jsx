7e76892e3d37485a34de5f7d148b288eada09be8
Elemento5-2026 <elemento5ag2026@outlook.com>
2026-08-01 21:26:50 -0600

    Update Encabezado.jsx

--- src/pages/NuevaInvestigacion/Components/Encabezado.jsx
+++ src/pages/NuevaInvestigacion/Components/Encabezado.jsx
@@ -10,7 +10,7 @@ export default function Encabezado() {
 
   const [gerencias, setGerencias] = useState([]);
 
-  const [formulario, setFormulario({
+  const [formulario, setFormulario] = useState({
 
     codigo_controlado: "",
 
