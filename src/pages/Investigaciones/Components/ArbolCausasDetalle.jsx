c6e3ea763e0d518868c25a3e6d7c682c2c98b03c
Elemento5-2026 <elemento5ag2026@outlook.com>
2026-08-05 18:48:49 -0600

    Add ArbolCausasDetalle component with placeholder content

---
+++ src/pages/Investigaciones/Components/ArbolCausasDetalle.jsx
@@ -0,0 +1,15 @@
+export default function ArbolCausasDetalle() {
+
+  return (
+
+    <div className="card">
+
+      <h3>Análisis de causas</h3>
+
+      <p>Próximamente...</p>
+
+    </div>
+
+  );
+
+}
